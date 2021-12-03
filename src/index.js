18501b22837d488ced803b4a75d8f87d52d248f0
Presy Lord Kidlat <91861180+presylord@users.noreply.github.com>
2021-12-04 05:35:22 +0800

    minor update

--- src/index.js
+++ src/index.js
@@ -3,3 +3,26 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+// import React, { useState } from "react";
+
+// function App() {
+//   const [count, setCount] = useState(0);
+
+//   function increase() {
+//     setCount(count + 1);
+//   }
+//   function decrease() {
+//     setCount(count - 1);
+//   }
+
+//   return (
+//     <div className="container">
+//       <h1>{count}</h1>
+//       <button onClick={decrease}>-</button>
+//       <button onClick={increase}>+</button>
+//     </div>
+//   );
+// }
+
+// export default App;
